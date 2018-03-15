c3de7b097cebc54ffdabf50462d944b946dcc1f1
Guad <rockeurp@gmail.com>
2018-03-15 17:20:07 +0100

    initial commit

---
+++ Clipboard.cs
@@ -0,0 +1,23 @@
+﻿namespace LastPassAnywhere
+{
+    public static class Clipboard
+    {
+        public static bool TryCopy(string val)
+        {
+            if (OperatingSystem.IsWindows)
+            {
+                $"echo {val.Replace("&", "^^^&").Replace("|", "^^^|")} | clip".Bat();
+            }
+            else if (OperatingSystem.IsMac)
+            {
+                $" echo \"{val}\" | pbcopy".Bash();
+            }
+            else if (OperatingSystem.IsLinux)
+            {
+                $" echo \"{val}\" | xclip -selection c".Bash();
+            }
+            else return false;
+            return true;
+        }
+    }
+}
---
+++ OperatingSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LastPassAnywhere
+{
+    public static class OperatingSystem
+    {
+        public static bool IsWindows =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMac =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsLinux =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    }
+}
